c1d983635261a71546a87ef203d4e9708dd2f3a8
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 17:58:53 +0800

    Add files via upload

--- Script-003-Boxplots.py
+++ Script-003-Boxplots.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[46]:
-
-
+#
 import seaborn as sb
 from matplotlib import pyplot as plt
 import pandas as pd
 import os
+#
 os.chdir('/Users/pauline/Documents/Python')
 dfB = pd.read_csv("Tab-Bathy.csv")
 dfB.head(5)
+#
 sb.set_style('whitegrid')
 sb.boxplot(data=dfB, orient="v", palette='coolwarm', saturation=1, 
            width=0.8, dodge=True, fliersize=5, linewidth=0.2, 
@@ -20,4 +19,3 @@ plt.xticks(rotation=45)
 plt.yticks(rotation=45)
 plt.title('Box-and-whisker plot for the Mariana Trench bathymetry', fontsize=12, fontfamily='sans-serif')
 plt.show()
-
