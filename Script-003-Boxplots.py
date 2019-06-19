35a0a087f43fe09f39286b65fc6d6d06d8e2d6d7
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 19:39:50 +0800

    Add files via upload

--- Script-003-Boxplots.py
+++ Script-003-Boxplots.py
@@ -1,21 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-#
+import os
+import pandas as pd
 import seaborn as sb
 from matplotlib import pyplot as plt
-import pandas as pd
-import os
-#
+
+sb.set_style('whitegrid')
+sb.set_context('paper')
+
 os.chdir('/Users/pauline/Documents/Python')
 dfB = pd.read_csv("Tab-Bathy.csv")
-dfB.head(5)
-#
-sb.set_style('whitegrid')
-sb.boxplot(data=dfB, orient="v", palette='coolwarm', saturation=1, 
-           width=0.8, dodge=True, fliersize=5, linewidth=0.2, 
-           whis=5, notch=True, ax=None)
+
+# define variables and plotting
+sb.boxplot(
+           data=dfB, orient="v", palette='coolwarm', saturation=1,
+           width=0.8, dodge=True, fliersize=5, linewidth=0.2,
+           whis=5, notch=True, ax=None
+           )
 sb.despine(offset=10, trim=True) # to offset the spines away from the bathymetric data
 plt.xticks(rotation=45)
 plt.yticks(rotation=45)
-plt.title('Box-and-whisker plot for the Mariana Trench bathymetry', fontsize=12, fontfamily='sans-serif')
+plt.title('Box-and-whisker plot for the Mariana Trench bathymetry',
+          fontsize=12, fontfamily='sans-serif')
+
+# printing and saving a file
+plt.tight_layout()
+plt.savefig('plot_Boxplot.png', dpi=300)
 plt.show()
